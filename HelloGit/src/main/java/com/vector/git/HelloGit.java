f66cc99c515f871a7eaf073a2d0b2a5918b30503
229250919 <229250919@qq.com>
2021-04-24 11:16:49 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/vector/git/HelloGit.java
@@ -0,0 +1,13 @@
+package com.vector.git;
+
+/**
+ * @author Oreo
+ * @create 2021-04-23 20:14
+ */
+public class HelloGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("HelloGit!");
+    }
+}
